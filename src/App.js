865ffc0be15cf39b707f2a7c5399f7d96e0a8e54
Thomas Sherer <tcpma2011@gmail.com>
2020-10-01 14:29:15 -0700

    Completed working demo code in App.js. Now beginning refactor to lab code.

--- src/App.js
+++ src/App.js
@@ -4,27 +4,34 @@ import './App.css';
 export default function App() {
   // banner = 'Life Odometer - rough road ahead, your mileage may vary'
   let username = "Bob"
-
+  
   function getLocation() {
     return "Stuck between a rock and a hard place..."
-  }
-
+  }    
+  
+  
+  // Header Component
+  //   text value via props
   function Header(props) {
     return (
       <header>
         <p>{props.name}'s view from the top!</p>
-      </header>
+      </header>        
     )
-  }
+  }    
+  
 
+  // Footer Component
+  //   trademark value via props
+  {/* <p>&copy 2020, vorSherer Creations</p> */}
   function Footer(props) {
     return (
       <footer>
         <p>Greetings, {props.name}, from the footer of my page</p>
         <p>&copy 2020, vorSherer Creations</p>
-      </footer>
+      </footer>        
     )
-  }
+  }    
 
   return (
     <>
@@ -34,20 +41,21 @@ export default function App() {
         <h2>Proof of Life!</h2>
         <h3>{getLocation()}</h3>
       </div>
+      <div>
+        <MoodToday mood='Happy' />
+      </div>
       <aside>
         <h3>Just an aside here...</h3>
       </aside>
       <Footer name="Earthling" />
-    </>
+    </>        
   );
-}
-
-// Header Component
-//   text value via props
+}    
 
 // Odometer Component
 
 // format - 4 digits, leading zeroes required
+// format d
 
 // button - incr odo by 1
 // button - incr odo by 10
@@ -56,8 +64,31 @@ export default function App() {
 
 // if odo > 9999, then odo -= 10000
 
-// Footer Component
-//   trademark value via props
-{/* <p>&copy 2020, vorSherer Creations</p> */}
-
 // Update state in a "React Way"
+
+  class MoodToday extends React.Component {
+
+    constructor() {
+      super();
+      this.state = {
+        mood: 'neutral'
+      }
+    }
+    
+    changeMood(change) {
+      this.setState({mood: 'Happy'}
+      )
+    }
+
+
+    render() {
+      return (
+        <div>
+          <h3>Today is a... {this.state.mood} day.</h3>
+          <button onClick={() => this.changeMood(change: 'improve')}>Improve</button>
+          <button onClick={() => this.changeMood(change: 'reduce')}>Reduce</button>
+        </div>
+      )
+    };
+  }
+
