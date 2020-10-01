d8c14bced0a52183272a4390869e26f09ccb8411
Thomas Sherer <tcpma2011@gmail.com>
2020-10-01 11:19:18 -0700

    Added requirements comments to App.js, README content, and renamed stock App.css to Rjs.css and replaced with new App.css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  // banner = 'Life Odometer - rough road ahead, your mileage may vary'
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+      </div>
+    </>
   );
 }
 
-export default App;
+// Header Component
+//   text value via props
+
+// Odometer Component
+
+// format - 4 digits, leading zeroes required
+
+// button - incr odo by 1
+// button - incr odo by 10
+// button - incr odo by 100
+// button - incr odo by 1000
+
+// if odo > 9999, then odo -= 10000
+
+// Footer Component
+//   trademark value via props
+{/* <p>&copy 2020, vorSherer Creations</p> */}
+
+// Update state in a "React Way"
