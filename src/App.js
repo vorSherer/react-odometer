6e4995a19856f303dead0289e3ebd3825cdda9b8
Thomas Sherer <tcpma2011@gmail.com>
2020-10-01 20:14:33 -0700

    Added decrement capability.

--- src/App.js
+++ src/App.js
@@ -57,19 +57,17 @@ class Footer extends React.Component {
       return odoDisplay.toString().padStart(4, '0');
     }
     
-    incrementOdo(change) {
+    updateOdo(change) {
       let odoDisplay = this.state.odometerValue
       let newOdoValue = odoDisplay + change
       // Prevent rolling odometer backwards
-      if (odoDisplay < 0) {
-        odoDisplay = 0
+      console.log('Display = ', odoDisplay)
+      if (newOdoValue < 0) {
+        newOdoValue = 0
       }
       
       // rolloverHandler
-      console.log('Going in ', newOdoValue)
-
       if (newOdoValue > 9999) {
-        console.log('Got this far ', newOdoValue)
         newOdoValue = newOdoValue - 10000
       }
       
@@ -84,10 +82,16 @@ class Footer extends React.Component {
         <>
           <div>
           <h1>Odometer value reads: {this.formatOdometerDisplay()}</h1>
-          <button onClick={() => this.incrementOdo(1)}>+1</button>
-          <button onClick={() => this.incrementOdo(10)}>+10</button>
-          <button onClick={() => this.incrementOdo(100)}>+100</button>
-          <button onClick={() => this.incrementOdo(1000)}>+1000</button>
+          <button onClick={() => this.updateOdo(1)}>+1</button>
+          <button onClick={() => this.updateOdo(10)}>+10</button>
+          <button onClick={() => this.updateOdo(100)}>+100</button>
+          <button onClick={() => this.updateOdo(1000)}>+1000</button>
+          </div>
+          <div>
+          <button onClick={() => this.updateOdo(-1)}>-1</button>
+          <button onClick={() => this.updateOdo(-10)}>-10</button>
+          <button onClick={() => this.updateOdo(-100)}>-100</button>
+          <button onClick={() => this.updateOdo(-1000)}>-1000</button>
           </div>
         </>            
       );
